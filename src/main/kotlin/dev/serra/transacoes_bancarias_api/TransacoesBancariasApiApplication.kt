800656f6618a989c6cd44168c394ec77c6a5bf85
lucas serra <lucasserradevelopment@gmail.com>
2025-02-12 21:05:27 -0300

    Iniciando projeto

---
+++ src/main/kotlin/dev/serra/transacoes_bancarias_api/TransacoesBancariasApiApplication.kt
@@ -0,0 +1,11 @@
+package dev.serra.transacoes_bancarias_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TransacoesBancariasApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<TransacoesBancariasApiApplication>(*args)
+}
---
+++ src/test/kotlin/dev/serra/transacoes_bancarias_api/TransacoesBancariasApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.serra.transacoes_bancarias_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TransacoesBancariasApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
