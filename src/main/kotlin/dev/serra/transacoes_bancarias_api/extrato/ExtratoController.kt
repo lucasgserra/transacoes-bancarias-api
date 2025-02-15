ae36415127f722d19f466905b0d0d6976732f220
lucas serra <lucasserradevelopment@gmail.com>
2025-02-15 13:20:56 -0300

    Endpoint de retornar um unico extrato criado

--- src/main/kotlin/dev/serra/transacoes_bancarias_api/extrato/ExtratoController.kt
+++ src/main/kotlin/dev/serra/transacoes_bancarias_api/extrato/ExtratoController.kt
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -17,5 +18,13 @@ class ExtratoController(private val service: ExtratoService) {
     ])
     @GetMapping
     fun pegarExtratos(): List<Extrato> = service.pegarExtratos()
+
+    @Operation(summary = "Retorna o objeto do extrato correspondente")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "404", description = "Extrato nao encontrado"),
+        ApiResponse(responseCode = "200", description = "Extrato encontrado"),
+    ])
+    @GetMapping("/{id}")
+    fun pegarExtratoID(@PathVariable id: String) = service.pegarExtratoID(id)
 
 }
--- src/main/kotlin/dev/serra/transacoes_bancarias_api/extrato/ExtratoService.kt
+++ src/main/kotlin/dev/serra/transacoes_bancarias_api/extrato/ExtratoService.kt
@@ -1,10 +1,18 @@
 package dev.serra.transacoes_bancarias_api.extrato
 
+import dev.serra.transacoes_bancarias_api.exceptions.NotFoundEntity
+import dev.serra.transacoes_bancarias_api.usuario.UsuarioService
+import org.hibernate.query.sqm.tree.SqmNode.log
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class ExtratoService(private val extratoRepository: ExtratoRepository) {
+
+    private val log: Logger = LoggerFactory.getLogger(ExtratoService::class.java)
 
     @EventListener
     fun registrarExtrato(event: Extrato) {
@@ -18,4 +26,16 @@ class ExtratoService(private val extratoRepository: ExtratoRepository) {
     }
 
     fun pegarExtratos(): List<Extrato> = extratoRepository.findAll()
+
+    fun pegarExtratoID(id: String): Extrato {
+        log.info("Iniciando processamente de extrato")
+        val extrato: Optional<Extrato> = extratoRepository.findById(UUID.fromString(id))
+        if (extrato.isPresent) {
+            log.info("Extrato encontrado")
+            return extrato.get()
+        } else {
+            log.error("ID de Extrato nao foi encontrado")
+            throw NotFoundEntity("ID de Extrato nao encontrado")
+        }
+    }
 }
